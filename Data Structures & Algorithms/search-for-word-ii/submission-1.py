66d2b8e6c9552455633343d56ea992372c30fb53
VaidehiMuley <muleyvaidehi11@gmail.com>
2026-07-24 20:03:46 +0530

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,54 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+    
+    def addWord(self,word):
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        for w in words:
+            root.addWord(w)
+
+        ROWS = len(board)
+        COLS = len(board[0])
+        visit = set()
+        res = set()
+
+        def dfs(r,c,node, wordsofar):
+            ## Base case:
+            
+
+            if (r < 0 or c < 0 or c >=COLS or r >= ROWS or board[r][c] not in node.children or (r,c) in visit):
+                return 
+        
+            visit.add((r,c))
+            node = node.children[board[r][c]]
+            wordsofar += board[r][c]
+            if node.endOfWord:
+                res.add(wordsofar)
+
+            dfs(r+1,c,node,wordsofar) 
+            dfs(r-1,c,node,wordsofar)
+            dfs(r,c+1,node,wordsofar)
+            dfs(r,c-1,node,wordsofar)
+            visit.remove((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,root, "")
+        return list(res)
+            
+
+
+
+
+        
